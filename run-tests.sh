303420c43c0675c270b2a87b642990908335a850
chaslinux <chaslinux@gmail.com>
2023-02-07 09:09:38 -0500

    Added if wrapper to only install pts if it isn't already installed. Also added comments.

--- run-tests.sh
+++ run-tests.sh
@@ -3,10 +3,12 @@
 # First install all updates
 sudo apt update && sudo apt upgrade -y
 
-# Updated to 10.8.4 on August 22, 2022
-wget -O phoronix.deb https://phoronix-test-suite.com/releases/repo/pts.debian/files/phoronix-test-suite_10.8.4_all.deb
-sudo dpkg -i phoronix.deb
-sudo apt --fix-broken install -y
+# Wrapped this in if statement to only install of pts isn't present already
+if [ ! /usr/bin/phoronix-test-suite ]; then
+	wget -O phoronix.deb https://phoronix-test-suite.com/releases/repo/pts.debian/files/phoronix-test-suite_10.8.4_all.deb
+	sudo dpkg -i phoronix.deb
+	sudo apt --fix-broken install -y
+fi
 
 if [ ! ~/.phoronix-test-suite ]
 	then
@@ -16,4 +18,5 @@ fi
 cp -R benchmarks.txt installed-tests/ ~/.phoronix-test-suite
 rm phoronix.deb
 
+# run benchmarks against our 1st gen Core iSeries laptop
 phoronix-test-suite benchmark 2204138-TJ-LAPTOP50755
